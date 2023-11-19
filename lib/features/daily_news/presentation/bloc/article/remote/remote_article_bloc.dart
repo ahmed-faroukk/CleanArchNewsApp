5a5c5150968b14d50c924aa6b89c9e11a4af3806
farouk <ahmed.farouk.334455@gmail.com>
2023-11-19 20:32:30 +0200

    bloc

---
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/core/resources/data_state.dart';
+import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
+import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
+
+import '../../../../domain/usecase/getArticleUseCase.dart';
+
+class RemoteArticleBloc extends Bloc<RemoteArticleEvents , RemoteArticleState>{
+
+  final GetArticleUseCase getArticleUseCase ;
+  
+  RemoteArticleBloc(this.getArticleUseCase) : super(const RemoteArticleLoading()) {
+    on<GetArticles> (onGetArticle) ;
+  }
+
+  void onGetArticle(GetArticles event , Emitter<RemoteArticleState> emit) async {
+    final dataState = await getArticleUseCase.invoke() ;
+    if(dataState is Success && dataState.data!.isNotEmpty){
+      emit(
+        RemoteArticleDone(dataState.data!)
+      );
+    }
+    
+    if(dataState is Error){
+      emit(
+        RemoteArticleError(dataState.error!)
+      );
+    }
+  }
+
+
+}
